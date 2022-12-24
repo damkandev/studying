6d82006225782d72d1672f5bf34b2ac0a36ffe0d
dampokan <panes.damianalberto@insucochillan.cl>
2022-12-24 19:24:47 -0300

    24-dec

---
+++ javascript/JAVASCRIPT-DEC-24/adarrayandobjects/script.js
@@ -0,0 +1,17 @@
+// function howMannyLetters(text){
+//  let resultado = text.length
+//  return resultado
+// }
+
+// let a = howMannyLetters("hola")
+// console.log(a)
+
+// TO-Do: hacer un for que recorra el array y los vaya sumando
+// el resultado debe ser 10
+let numeros = [1, 2, 3, 4]
+let resultado = 0
+
+for (let numero of numeros){
+  resultado = resultado + numero
+  console.log(resultado)
+}
