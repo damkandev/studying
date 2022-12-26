f062969aeb3c7d68e7af9d289f46019b2b8604e7
Damián Panes <dapcc6designer@gmail.com>
2022-12-26 13:55:26 -0300

    dec-26

--- javascript/JAVASCRIPT-DEC-24/adarrayandobjects/script01.js
+++ javascript/JAVASCRIPT-DEC-24/adarrayandobjects/script01.js
@@ -2,5 +2,18 @@
 // ej: haha => ({'a':2},{'h':2})
 
 const letterFre = (frase) => {
+    let resultado = []
     
+    for(let i = 0; i < frase.length; i++){
+        console.log("x")
+        /*for(let o = 0; i < frase.length; o++){
+            if(frase[i] == frase[o]){
+                resultado.push(frase[i], frase[o])
+                console.log(resultado)
+            }
+        }*/
+    }
+    return (frase.length)
 }
+
+console.log(letterFre("haha"))
